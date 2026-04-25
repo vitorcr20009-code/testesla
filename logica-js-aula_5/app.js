528f10f362c903bd009f5b6b9192af503cc1030a
Vitor da silva <Vitorcr20009@gmail.com>
2026-04-25 18:50:38 -0300

    deixando mais dificl

--- logica-js-aula_5/app.js
+++ logica-js-aula_5/app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 500;
+let numeroMaximo = 1000;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
